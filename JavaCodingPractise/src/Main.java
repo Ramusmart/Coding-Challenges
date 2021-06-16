fcd690356da235e54ba4c80f6d6d38e5f3b1bb48
RamaswamiM193 <Ramaswami.Shakuntala@Surveysampling.com>
2021-06-17 02:17:14 +0530

    Stack and queue implementation

--- JavaCodingPractise/src/Main.java
+++ JavaCodingPractise/src/Main.java
@@ -12,6 +12,29 @@ public class Main {
 		//dup.duplicateOfArrayUsingCollections();
 		
 		removeDup.removeDuplicates();
+		
+		//Stack implementation : 
+		/*
+		 * StackImpl stc = new StackImpl(10);
+		 * System.out.println("Is stack empty : "+stc.isEmpty());
+		 * System.out.println("Is stack full : "+stc.isFull());
+		 * System.out.println("Peek : "+stc.peek()); stc.push(10); stc.push(1);
+		 * stc.push(20); stc.push(50); stc.displayArray(); stc.pop();
+		 * stc.displayArray();
+		 */
+		//System.out.println("elemetns "+stc.arr);
+		
+		LinkedListStack lls = new LinkedListStack();
+		//System.out.println(lls.pop());
+		lls.push(10);
+		lls.push(20);
+		lls.push(60);
+		System.out.println("poped:"+lls.pop());
+		lls.push(50);
+		System.out.println("poped:"+lls.pop());
+		lls.printList(lls.head);
+		
+		
 	}
 
 }
